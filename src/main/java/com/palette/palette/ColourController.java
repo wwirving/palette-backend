d836628b86a37cadf1d77d7523a1934069270539
Will Irving <willirving@Wills-Mac-mini.local>
2021-07-15 15:45:03 +0100

    hosting for colourDB completed

--- src/main/java/com/palette/palette/ColourController.java
+++ src/main/java/com/palette/palette/ColourController.java
@@ -2,9 +2,9 @@ package com.palette.palette;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -13,9 +13,41 @@ public class ColourController {
 	@Autowired
 	public IColourRepository repository;
 
-	public ColourController(){
+	public ColourController() {
 
 	}
 
-	@GetMapping("/courses")
+	@GetMapping("/colours")
+	public ResponseEntity<Colour> getColours() {
+		return new ResponseEntity(this.repository.findAll(), HttpStatus.OK);
+	}
+
+	//get single colour by ID
+	@GetMapping("/colours/{colourId}")
+	public ResponseEntity getColour(@PathVariable String colourId) {
+		Colour colour = this.repository.findById(colourId).get();
+
+		if (colour != null) {
+			return new ResponseEntity<>(colour, HttpStatus.OK);
+		} else {
+			return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@PostMapping("/colours")
+	public ResponseEntity<Colour> createColour(@RequestBody Colour colour) {
+		Colour dbColour = this.repository.save(colour);
+		if (dbColour != null) {
+			return ResponseEntity.status(HttpStatus.OK).body(colour);
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(colour);
+		}
+	}
+
+	@DeleteMapping("/colours/{colourId}")
+	public ResponseEntity deleteColour(@PathVariable String colourId) {
+		this.repository.deleteById(colourId);
+		return ResponseEntity.status(HttpStatus.OK).body("Succesfully deleted colour");
+	}
 }
+
